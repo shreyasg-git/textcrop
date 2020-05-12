eb4dd0231996659660fe12d4972d36505766068f
Shreyas Gangurde <57534229+shreyasg-git@users.noreply.github.com>
2020-05-13 01:07:59 +0530

    Done with margins !

--- trial9(2).py
+++ trial9(2).py
@@ -1,20 +1,22 @@
 import cv2
-img = cv2.imread('img1.png')
+img = cv2.imread('img3.png')
+
 imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
 # ret, thresh = cv2.threshold(imgray, 0, 120, 0)
 ret, thresh = cv2.threshold(imgray, 127, 255, 4)
-contours, _ = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-cv2.imshow("thresh", thresh)
 
-xs = []
-ys = []
-xws = []
-yhs = []
+contours, _ = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+# cv2.imshow("thresh", thresh)
+
+xs = []     # The x-coordinate of each
+ys = []     # The y-coordinate of each
+xws = []    # The (x+w)-coordinate of each
+yhs = []    # The (y+h)-coordinate of each
 
 for c in contours:
+
     rect = cv2.boundingRect(c)
-    # if rect[2] < 100 or rect[3] < 100: continue
-    print(cv2.contourArea(c))
     x, y, w, h = rect
     cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 1)
 
@@ -24,6 +26,7 @@ for c in contours:
     yhs.append(y+h)
 
     # cv2.putText(img, 'Moth Detected', (x+w+10,y+h), 0, 0.3, (0, 255, 0))
+
 # print(min(starts))
 # print(starts)
 # print(max(ends))
@@ -35,6 +38,8 @@ right_down = (max(xws), max(yhs))
 # left_up = (min(ys), min(xs))
 # right_down = (max(yhs), max(xws))
 
+final_image = img[left_up[1] - 7:right_down[1] + 7, left_up[0] - 7:right_down[0] + 7]
+cv2.imwrite('first_final.png', final_image)
 
 
 cv2.rectangle(img, left_up, right_down, (0, 255, 255), 1)
@@ -43,12 +48,3 @@ cv2.imshow("Gray", imgray)
 cv2.waitKey()
 cv2.destroyAllWindows()
 
-"""
-Threshold Flags (4th argument)
-https://docs.opencv.org/master/d7/d4d/tutorial_py_thresholding.html
-    0 - cv.THRESH_BINARY  
-    1 - cv.THRESH_BINARY_INV
-    2 - cv.THRESH_TRUNC
-    3 - cv.THRESH_TOZERO
-    4 - cv.THRESH_TOZERO_INV == Best for black text on white bg
-"""
